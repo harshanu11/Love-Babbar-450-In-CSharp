0319f342b2d43a3b5287b7814c9a5aba95b724ff
harsh singh <harshanu11@gmail.com>
2021-09-14 18:52:23 +0530

    adding console to app

---
+++ Love-Babbar-450-Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Love_Babbar_450_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
