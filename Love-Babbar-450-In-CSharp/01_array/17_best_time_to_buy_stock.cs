ae6bb73ac90e2a2921f3136f84ea6b2f6db3b60a
harsh singh <harshanu11@gmail.com>
2021-09-19 14:23:16 +0530

    array update

--- Love-Babbar-450-In-CSharp/01_array/17_best_time_to_buy_stock.cs
+++ Love-Babbar-450-In-CSharp/01_array/17_best_time_to_buy_stock.cs
@@ -49,3 +49,25 @@ namespace _01_array
 		[Fact] public void Test() { }
     }
 }
+/*
+    link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+    also refer to 26_sell_...twice.cpp
+*/
+
+
+//// ----------------------------------------------------------------------------------------------------------------------- //
+//int maxProfit(vector<int>& prices)
+//{
+//	int n = prices.size();
+//	int local = 0, global = 0;
+//	for (int i = 1; i < n; i++)
+//	{
+//		/*
+//            if the loss is going negative then make it 0 either continue with the profit as higher as possible.
+//        */
+//		local = max(0, local += prices[i] - prices[i - 1]);
+//		global = max(local, global);
+//	}
+//	return global;
+//}
--- Love-Babbar-450-In-CSharp/01_array/30_choclate_distribution_problem.cs
+++ Love-Babbar-450-In-CSharp/01_array/30_choclate_distribution_problem.cs
@@ -29,3 +29,23 @@ is 9 - 3 = 6 by choosing following M packets :
         [Fact] public void Test() { }
     }
 }
+/*
+    link: https://practice.geeksforgeeks.org/problems/chocolate-distribution-problem3825/1
+*/
+
+
+//// ----------------------------------------------------------------------------------------------------------------------- //
+//long long findMinDiff(vector<long long> a, long long n, long long m)
+//{
+//    // sorted so that we can different window(sub-array) of size m.
+//    sort(a.begin(), a.end());
+
+//    long long ans = a[m - 1] - a[0];    // took first window from 0 to m-1.
+
+//    // now we iterate every window(sub-array) and check for the max-min from that range.
+//    for (int i = 1; i < n - m + 1; i++)
+//    {
+//        ans = min(a[i + m - 1] - a[i], ans);
+//    }
+//    return ans;
+//}
