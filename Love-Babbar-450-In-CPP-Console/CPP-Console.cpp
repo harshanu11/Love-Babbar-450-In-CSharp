29d84c652612223b461e1e265b95c0749fe8c94e
harsh singh <harshanu11@gmail.com>
2021-09-29 12:28:43 +0530

    creating function

--- Love-Babbar-450-In-CPP-Console/CPP-Console.cpp
+++ Love-Babbar-450-In-CPP-Console/CPP-Console.cpp
@@ -1,11 +1,15 @@
-// Love-Babbar-450-In-CPP-Console.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include <cmath>// for math opration
 #include <string>// get user input as string 
 using namespace std;
-
+void SayHi(string name)
+{
+	cout << "\nhello user"<< name;
+}
+int GiveCube(int num)
+{
+	return  num * num * num;
+}
 int main()
 {
 	// data type 
@@ -61,17 +65,9 @@ int main()
 	arr[10] = 55;// can not be index out on range
 #pragma endregion
 
-
+	SayHi("mu new funciton ");
+	cout << GiveCube(3);
 	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
