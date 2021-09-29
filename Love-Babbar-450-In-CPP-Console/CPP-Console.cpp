e0ce42f7b1afb1d5935bc1d9014015e042d817c1
harsh singh <harshanu11@gmail.com>
2021-09-29 11:51:35 +0530

    addin user input

--- Love-Babbar-450-In-CPP-Console/CPP-Console.cpp
+++ Love-Babbar-450-In-CPP-Console/CPP-Console.cpp
@@ -2,23 +2,60 @@
 //
 
 #include <iostream>
+#include <cmath>// for math opration
+#include <string>// get user input as string 
 using namespace std;
 
 int main()
 {
-    // data type 
-    bool isMail = true;
-    char alphabat = 'A';
-    int age = 23;
-    float profit = 22.3;
-    double  gpa = 4.5;
-    std::string strUsingNameSpace = "the way to add namespace";
+	// data type 
+	bool isMail = true;
+	char alphabat = 'A';
 
-    // console section
-    cout << "Hello World !"<< strUsingNameSpace<< "my age is " << age <<endl;
-    cout << "dfgdg" << strUsingNameSpace << endl;
-    cout << "hello" << endl;
-    return 0;
+	int age = 23;
+	float profit = 22.3;
+	double  gpa = 4.5;
+#pragma region Number
+	cout << 10 % 3;
+	age++;
+	++age;
+	cout << 15.5 - 5;
+	cout << pow(2, 5);//32 2^5
+	cout << sqrt(2.5);//
+	cout << round(4.5);//
+	cout << ceil(4.5);//
+	cout << fmax(4.5, 8);//
+	cout << fmin(4.5, 8);//
+
+#pragma endregion
+
+
+	std::string strUsingNameSpace = "the way to add namespace";
+	// string fn 
+#pragma region String Fn 
+	strUsingNameSpace.length();
+	char dsf = strUsingNameSpace[2];
+	strUsingNameSpace.find('d');
+	strUsingNameSpace.find("sfsf");
+	strUsingNameSpace.find("sfsf", 5);// look from index 5
+	strUsingNameSpace.substr(8, 5);// start , till length
+#pragma endregion
+
+	// console section
+	cout << "Hello World !" << strUsingNameSpace << "my age is " << age << endl;
+	cout << "dfgdg" << strUsingNameSpace << endl;
+	cout << "hello" << endl;
+	cout << "Want new line\n";
+	cout << "Part one ";
+	cout << "+ part two";
+
+#pragma region UserInput
+	string str;
+	cin >> age;
+	getline(cin, str);
+#pragma endregion
+
+	return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
