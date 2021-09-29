45dc00820af2689a28978f53f215e12168a3c72f
harsh singh <harshanu11@gmail.com>
2021-09-29 10:08:29 +0530

    adding c++ console and test

---
+++ Love-Babbar-450-In-CPPTest/Love-Babbar-450-In-CPPTest.cpp
@@ -0,0 +1,21 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace LoveBabbar450InCPPTest
+{
+	TEST_CLASS(LoveBabbar450InCPPTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			std::string charM = "harhs";
+			int age = 14;
+			age = 55;
+			std::string lastName = "<<charM <<singh";
+			
+		}
+	};
+}
