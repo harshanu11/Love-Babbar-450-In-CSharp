8810debe529ac6531b2cfb4d05939ecc8f91e2fd
harsh singh <harshanu11@gmail.com>
2021-10-20 12:35:43 +0530

    array test complete

--- Love-Babbar-450-In-CPPTest/01_array/01_reverse_array.cpp
+++ Love-Babbar-450-In-CPPTest/01_array/01_reverse_array.cpp
@@ -1,7 +1,7 @@
 /*
-    link: https://www.geeksforgeeks.org/write-a-program-to-reverse-an-array-or-string/
+	link: https://www.geeksforgeeks.org/write-a-program-to-reverse-an-array-or-string/
 
-    note: array is pass by reference so no need to worry while changing value it will change in original array.
+	note: array is pass by reference so no need to worry while changing value it will change in original array.
 */
 //#include "pch.h"
 #include "CppUnitTest.h"
@@ -18,52 +18,64 @@
 using namespace std;
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace LoveBabbar450InCPPTest
 {
-    TEST_CLASS(ArrayTest)
-    {
-    public:
+	TEST_CLASS(ArrayTest)
+	{
+	public:
 
-        TEST_METHOD(RevrseTest)
-        {
-            std::string charM = "harhs";
-            int age = 14;
-            age = 55;
-            std::string lastName = "<<charM <<singh";
+		TEST_METHOD(RevrseTest)
+		{
+			int arr[] = { 1000, 11, 445, 1, 330, 3000 };
+			rvereseArrayRecur(arr, 0, 5);
+			rvereseArray1(arr, 0, 5);
+			rvereseArrayBit(arr, 0, 5);
 
-        }
-    };
+
+		}
+		// ----------------------------------------------------------------------------------------------------------------------- //
+		// TC: O(N)
+		void rvereseArray1(int arr[], int start, int end)
+		{
+			while (start < end)
+			{
+				int temp = arr[start];
+				arr[start] = arr[end];
+				arr[end] = temp;
+				start++;
+				end--;
+			}
+		}
+
+
+		// ----------------------------------------------------------------------------------------------------------------------- //
+		// TC: O(N)
+		void rvereseArrayRecur(int arr[], int start, int end)
+		{
+			if (start >= end)
+				return;
+
+			int temp = arr[start];
+			arr[start] = arr[end];
+			arr[end] = temp;
+
+			// Recursive Function calling
+			rvereseArrayRecur(arr, start + 1, end - 1);
+		}
+		// ----------------------------------------------------------------------------------------------------------------------- //
+		// TC: O(N)
+		// TC: O(N)
+		void rvereseArrayBit(int arr[], int start, int end)
+		{
+			for (int a = 0; a < (start+end) / 2; a++)
+			{
+				arr[a] = arr[a] ^ arr[end - a];
+				arr[end - a] = arr[a] ^ arr[end - a];
+				arr[a] = arr[a] ^ arr[end  - a];
+			}
+		}
+	};
 }
 
 
-// ----------------------------------------------------------------------------------------------------------------------- //
-// TC: O(N)
-void rvereseArray1(int arr[], int start, int end)
-{
-    while (start < end)
-    {
-        int temp = arr[start];
-        arr[start] = arr[end];
-        arr[end] = temp;
-        start++;
-        end--;
-    }
-}
-
-
-// ----------------------------------------------------------------------------------------------------------------------- //
-// TC: O(N)
-void rvereseArray(int arr[], int start, int end)
-{
-    if (start >= end)
-        return;
-
-    int temp = arr[start];
-    arr[start] = arr[end];
-    arr[end] = temp;
-
-    // Recursive Function calling
-    rvereseArray(arr, start + 1, end - 1);
-}
