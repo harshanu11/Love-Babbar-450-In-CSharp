caa03188ec7c60a6bc0383f51ba0c36102a2b1f6
harsh singh <harshanu11@gmail.com>
2021-12-06 20:01:56 +0530

    all possible sub array

--- Love-Babbar-450-In-CSharp/01_array/basic/BasicTest.cs
+++ Love-Babbar-450-In-CSharp/01_array/basic/BasicTest.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 using Xunit;
 
 namespace _01_array.basic
@@ -12,7 +10,8 @@ namespace _01_array.basic
         {
             //var nums = new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
             var nums = new int[] { -2, -2 };
-            
+            subArray(nums);
+            AllContiguousPossibleSubArray(new int[] { 1, 2, 3 });
         }
         // AllContiguousPossibleSubArray count = n
         public void AllContiguousPossibleSubArray(int[] nums)
@@ -28,6 +27,20 @@ namespace _01_array.basic
                         s += "," + nums[j];
                     }
                     vs.Add(s);
+                }
+            }
+        }
+        List<List<int>> vs2 = new List<List<int>>();
+        public void subArray(int[] n)
+        {
+            for (int i = 0; i < n.Length; i++)
+            {
+                for (int j = i; j < n.Length; j++)
+                {
+                    List<int> temp = new List<int>();
+                    for (int k = i; k <= j; k++)
+                        temp.Add(n[k]);
+                    vs2.Add(temp);
                 }
             }
         }
