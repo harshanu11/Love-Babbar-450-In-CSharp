775ad88148129671e3a977c42c34b0f28e964ff4
harsh singh <harshanu11@gmail.com>
2021-12-16 20:02:07 +0530

    adding contains duplicate

--- Love-Babbar-450-In-CSharp/ConsoleTest.cs
+++ Love-Babbar-450-In-CSharp/ConsoleTest.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Text;
+﻿using System.Collections.Generic;
 using Xunit;
 // devide and concore
 //https://leetcode.com/problems/maximum-subarray/
 // sliding window 
-//https://leetcode.com/problems/contains-duplicate-ii/
 namespace ConsoleTest
 {
     public class ConsoleTest
@@ -15,54 +11,27 @@ namespace ConsoleTest
         public void reverse_arrayTest()
         {
             var nums = new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
-            ContainsNearbyDuplicate1(new int[] { 99, 99 }, 2);
+            ContainsNearbyDuplicate(new int[] { 99, 99 }, 2);
+            ContainsNearbyDuplicate(new int[] { 1, 2, 3, 1 }, 3);
+            ContainsNearbyDuplicate(new int[] { 1, 0, 1, 1 }, 1);
+            ContainsNearbyDuplicate(new int[] { 1, 2, 3, 1, 2, 3 }, 2);
+            //ContainsNearbyDuplicate1(new int[] { 1, 2, 3, 1, 2, 3 }, 2);
             //var nums = new int[] { -2, -2 };
         }
+        //https://leetcode.com/problems/contains-duplicate-ii/
         public bool ContainsNearbyDuplicate(int[] nums, int k)
         {
-            for (int i = 0; i < nums.Length; i++)
+            Dictionary<int, int> hashMap = new Dictionary<int, int>();
+            for (int i = 0; i< nums.Length; i++)
             {
-                for (int j = i + 1; j < nums.Length; j++)
+                if (hashMap.ContainsKey(nums[i]) && i-hashMap[nums[i]]<=k)
                 {
-                    if (nums[i] == nums[j])
-                    {
-                        if (Math.Abs(i - j) <= k)
-                        {
-                            return true;
-                        }
-                        Debug.WriteLine($"{i},{j}");
-                    }
+                    return true;
                 }
-            }
-            return false;
-        }
-        public bool ContainsNearbyDuplicate1(int[] nums, int k)
-        {
-            if (k > nums.Length)
-            {
+                hashMap[nums[i]] = i;
 
             }
-            int flagS = 0;
-            for (int i = flagS; i <= k; i++)
-            {
-                if (k < nums.Length && nums[flagS] == nums[k])
-                {
-                    if (Math.Abs(i - k) <= k)
-                    {
-                        return true;
-                    }
-                }
-                else if (i == k && k < nums.Length)
-                {
-                    flagS++;
-                    i = flagS;
-                    k++;
-                }
-                if (k == nums.Length)
-                {
-                    break;
-                }
-            }
+
             return false;
         }
     }
