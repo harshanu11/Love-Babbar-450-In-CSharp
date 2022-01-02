31f59f510191ba260f9eb8126c717b7230bf787e
harsh singh <harshanu11@gmail.com>
2022-01-02 15:17:10 +0530

    code update

--- Love-Babbar-450-In-CPP-Console/CPP-Console.cpp
+++ Love-Babbar-450-In-CPP-Console/CPP-Console.cpp
@@ -170,6 +170,30 @@ void makOfNo()
 	ans = ans & mask;
 	cout << ans;
 }
+bool twoPow() {
+	int n = 5;
+	int temp = 0;
+	for (int i = 0; i < 31; i++)
+	{
+		temp = pow(2, i);
+		if (temp == n) return true;
+		else if (temp > n)
+			return false;
+	}
+	return false;
+
+	int temp = 1;
+	for (int i = 0; i < 31; i++)
+	{
+		if (temp == n) return true;
+		else if (temp > n)
+			return false;
+		if (temp < INT_MAX / 2) //return false;
+			temp = 2 * temp;
+
+	}
+	return false;
+}
 void ConditionFunction()
 {
 	int age = 0;
