058ee7e3df7fc5c5ee3debd1cebdacc516cd01f5
harsh singh <harshanu11@gmail.com>
2022-01-04 19:42:48 +0530

    adding binary problem

--- Love-Babbar-450-In-CPP-Console/CPP-Console.cpp
+++ Love-Babbar-450-In-CPP-Console/CPP-Console.cpp
@@ -450,6 +450,32 @@ void swapAlternate(int arr[], int size) {
 	}
 
 }
+int binarySearch(int arr[], int size, int key) {
+
+	int start = 0;
+	int end = size - 1;
+
+	int mid = start + (end - start) / 2;
+
+	while (start <= end) {
+
+		if (arr[mid] == key) {
+			return mid;
+		}
+
+		//go to right wala part
+		if (key > arr[mid]) {
+			start = mid + 1;
+		}
+		else { //key < arr[mid]
+			end = mid - 1;
+		}
+
+		mid = start + (end - start) / 2;
+	}
+
+	return -1;
+}
 #pragma endregion
 
 
