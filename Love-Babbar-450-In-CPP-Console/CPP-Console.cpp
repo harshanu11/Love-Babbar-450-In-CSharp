ca1c23f180e0dd2b3cb2e429a42a5c7c45074e46
harsh singh <harshanu11@gmail.com>
2022-02-02 19:21:25 +0530

    tri addition

--- Love-Babbar-450-In-CPP-Console/CPP-Console.cpp
+++ Love-Babbar-450-In-CPP-Console/CPP-Console.cpp
@@ -263,7 +263,7 @@ void stlFn()
 void conditionFunction()
 {
 	int age = 0;
-	if (true)
+	if (true or true)
 	{
 
 	}
@@ -1307,11 +1307,11 @@ void priorityQueue_MaxHeapDemoLB()
 	// first element always gratest
 	//max heap
 	priority_queue<int> maxi;
-
+	maxi.empty();// true;
 	//min - heap
 	priority_queue<int, vector<int>, greater<int> > mini;
 
-	maxi.push(1);
+	maxi.push(1);// takes only log n
 	maxi.push(3);
 	maxi.push(2);
 	maxi.push(0);
@@ -2072,6 +2072,56 @@ void llCircular()
 
 	deleteNodeCircular(tail, 5);
 	printCircular(tail);
+}
+class Trie {
+public:
+	string word;
+	bool is_word = false;
+	Trie* children[26] = { NULL };
+};
+void insertTri(string& s, Trie* root) {
+	Trie* temp = root;
+	for (auto& i : s) {
+		int k = i - 'a';
+		if (temp->children[k] == NULL) {
+			temp->children[k] = new Trie;
+		}
+		temp = temp->children[k];
+	}
+	temp->word = s;
+	temp->is_word = true;
+}
+void dfsTri(vector<vector<char>>& board, vector<string>& res, Trie* temp, int i, int j) {
+
+	if (i < 0 || i >= board.size() || j < 0 || j >= board[0].size() || board[i][j] == '*' || !(temp->children[board[i][j] - 'a']))
+		return;
+
+	temp = temp->children[board[i][j] - 'a'];
+
+	if (temp->is_word) {
+		res.push_back(temp->word);
+		temp->is_word = false;
+	}
+
+	char temp_char = board[i][j];
+	board[i][j] = '*';
+
+	dfsTri(board, res, temp, i + 1, j);
+	dfsTri(board, res, temp, i, j + 1);
+	dfsTri(board, res, temp, i - 1, j);
+	dfsTri(board, res, temp, i, j - 1);
+
+	board[i][j] = temp_char;
+
+	return;
+}
+bool startsWith(string prefix, Trie* root) {
+	Trie* cur = root;
+	for (char c : prefix) {
+		if (cur->children[c - 'a'] == NULL)return false;
+		cur = cur->children[c - 'a'];
+	}
+	return true;
 }
 int main()
 {
